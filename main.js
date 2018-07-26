8865927bb86b745a2beab4e86c07731b3be97701
Razwanul Ghani <razwanulghani@gmail.com>
2018-07-26 18:29:52 +0600

    this is the very first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+new Vue({
+  el : '#app',
+  data:{
+    myWidth: 100,
+    monsterWidth: 100,
+    myDamage:0,
+    monsterDamage:0,
+    show: false,
+    attackLogs:[]
+  },
+  methods:{
+    start: function(){
+      if(this.show == false)
+        this.show = true;
+      else{
+        this.myWidth = 100;
+        this.monsterWidth = 100;
+        this.emptyArray(this.attackLogs);
+      }
+    },
+    attack: function(){
+      this.myDamage = Math.floor(this.getRandomAttribute(15, 0));
+      this.myWidth-=this.myDamage;
+      this.attackLogs.push('You are hurted '+this.myDamage+'points');
+      this.monsterDamage = Math.floor(this.getRandomAttribute(15, 0));
+      this.monsterWidth-=this.monsterDamage;
+      this.attackLogs.push('Monster is hurted '+this.monsterDamage+'points');
+      if(this.myWidth<=0){
+        alert('You Loose!');
+        this.start();
+      }if(this.monsterWidth<=0){
+        alert('You won! Congrats!');
+        this.start();
+      }
+    },
+    giveUp:function(){
+      alert('You loose!');
+      this.myWidth = 100;
+      this.monsterWidth = 100;
+      this.show = false;
+      this.emptyArray(this.attackLogs);
+    },
+    getRandomAttribute:function(max, min){
+      return Math.floor(Math.random()*(max-min)+min);
+    },
+    spcAttack:function(){
+      this.myDamage = Math.floor(this.getRandomAttribute(24, 0));
+      this.myWidth-=this.myDamage;
+      this.attackLogs.push('You are hurted '+this.myDamage+'points');
+      this.monsterDamage = Math.floor(this.getRandomAttribute(30, 0));
+      this.monsterWidth-=this.monsterDamage;
+      this.attackLogs.push('Monster is hurted '+this.monsterDamage+'points');
+      if(this.myWidth<=0){
+        alert('You Loose!');
+        this.start();
+      }if(this.monsterWidth<=0){
+        alert('You won! Congrats!');
+        this.start();
+      }
+    },
+    emptyArray: function(arr){
+      var length = arr.length;
+      for(var i = 0; i<length ;i++){
+        Vue.set(arr, i, '');
+      }
+    },
+    heal:function(){
+      this.myWidth+=Math.floor(this.getRandomAttribute(24, 0));
+      if(this.myWidth >= 100){
+        this.myWidth = 100;
+      }
+      this.attackLogs.push('Your health healed to '+this.myWidth+' points');
+      this.monsterWidth+=Math.floor(this.getRandomAttribute(24, 0));
+      if(this.monsterWidth >= 100){
+        this.monsterWidth = 100;
+      }
+      this.attackLogs.push('Monster health healed to '+this.myWidth+' points');
+    }
+  }
+});
